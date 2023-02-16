8037b185c817b24b23ca893ce917c35c5af9e4a8
FridayNoob <125467009+FridayNoob@users.noreply.github.com>
2023-02-16 12:09:17 +0800

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println( "Hello Gi3" );
         System.out.println( "Hello hot-fix" );
         System.out.println( "idea-update" );
+        System.out.println( "github-update" );
     }
 }
